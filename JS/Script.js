024deae34fcbc5b46c3213543fb9bfda9f53bb7f
Freecody15 <Dakota_pope1996@hotmail.com>
2022-03-14 19:50:13 -0400

    ADD
    Edited alert welcome name
    added my var favfood and value of what I like

--- JS/Script.js
+++ JS/Script.js
@@ -1,9 +1,13 @@
-// alert('Hello World');
-// let myName = 'Bob';
+alert('Welcome to the Pokedex');
+
+let favoriteFood = 'Fajitas';
+document.write(favoriteFood);
+
 // document.write(myName);
 // myName = 'John Doe';
 // document.write(myName);
 
+/* 
 let simpleAddition = 2 + 2;
 document.write(simpleAddition);
 
@@ -15,4 +19,5 @@ let size = 100;
 let doubleSize = size * 2;
 let minSize = (doubleSize * 2) - (size / 2);
 document.write(minSize);
+*/
 
