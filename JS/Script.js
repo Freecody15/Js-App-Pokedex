420d27294df0d45f686ff9a7e4cbe17ca1e87b8c
Freecody15 <Dakota_pope1996@hotmail.com>
2022-03-15 16:55:22 -0400

    Added loop / and Docu write
    Updated code to show information on pokeman depending on heights

--- JS/Script.js
+++ JS/Script.js
@@ -17,3 +17,20 @@ let pokemonList = [{
     type: ['fire']
 },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);  
+    if (pokemonList[i].height >= 1.7) {
+      document.write(' - Wow, that\'s a big Pokemon!');
+      if (pokemonList[i].height < 1.7 && pokemonList[i].height >= 1) {   
+        document.write(' - This is a medium size Pokemon!');
+      }
+      else if (pokemonList[i].height < 1) {
+        document.write(' - This is a small Pokemon!');
+      }
+      else if (pokemonList[i].height > 1.7){
+        document.write(' - This is a HUGE Pokemon!');
+      }
+
+    }
+}
