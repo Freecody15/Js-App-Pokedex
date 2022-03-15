7aa4fe741906831c1634d0f3f9ccb29ffd2cf597
Freecody15 <Dakota_pope1996@hotmail.com>
2022-03-14 22:00:39 -0400

    Fresh start
    Start of Pokemon list

--- JS/Script.js
+++ JS/Script.js
@@ -1,23 +1,19 @@
-alert('Welcome to the Pokedex');
+let pokemonList = [{
 
-let favoriteFood = 'Fajitas';
-document.write(favoriteFood);
+    name: 'Gyarados',
+    height: 6.5,
+    type: ['Water', 'flying']
+},
 
-// document.write(myName);
-// myName = 'John Doe';
-// document.write(myName);
+{
+    name: 'Pikachu',
+    height: 1.04,
+    type: ['electric']
+},
 
-/* 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-*/
-
+{
+    name: 'Ninetales',
+    height: 1.1,
+    type: ['fire']
+},
+];
