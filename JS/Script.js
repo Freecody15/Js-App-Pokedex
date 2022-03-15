22c950b4a2f2c44010213e7048b9a0bdeb04786d
Freecody15 <Dakota_pope1996@hotmail.com>
2022-03-15 14:08:31 -0400

    update for task 1.1

--- JS/Script.js
+++ JS/Script.js
@@ -1,19 +1,4 @@
-let pokemonList = [{
+alert('Hello World');
 
-    name: 'Gyarados',
-    height: 6.5,
-    type: ['Water', 'flying']
-},
-
-{
-    name: 'Pikachu',
-    height: 1.04,
-    type: ['electric']
-},
-
-{
-    name: 'Ninetales',
-    height: 1.1,
-    type: ['fire']
-},
-];
+let favoriteFood = 'Fajitas';
+document.write(favoriteFood);
