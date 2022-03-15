443b8afb735e8c3e816f6c552032501f07ebcec8
Freecody15 <Dakota_pope1996@hotmail.com>
2022-03-15 19:03:00 -0400

    cleaning code up

--- JS/Script.js
+++ JS/Script.js
@@ -1,21 +1,21 @@
-let pokemonList = [{
-
+let pokemonList = [
+    {
     name: 'Gyarados',
     height: 6.5,
     type: ['Water', 'flying']
 },
 
-{
+    {
     name: 'Pikachu',
     height: 1.04,
     type: ['electric']
-},
+    },
 
-{
+    {
     name: 'Ninetales',
     height: 1.1,
     type: ['fire']
-},
+    },
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
