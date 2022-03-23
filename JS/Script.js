52e4e42d1b1aca338b1563ee137f7bfc2b38fb09
Freecody15 <Dakota_pope1996@hotmail.com>
2022-03-23 11:22:32 -0400

    updated
    Added Repository, add function etc

--- JS/Script.js
+++ JS/Script.js
@@ -1,34 +1,50 @@
-let pokemonList = [
+let pokeRepository = (function () {
+  let pokemonList = [
     {
-    name: 'Gyarados',
-    height: 6.5,
-    type: ['Water', 'flying']
-},
-
-    {
-    name: 'Pikachu',
-    height: 1.04,
-    type: ['electric']
+      name: 'Gyarados',
+      height: 6.5,
+      type: ['Water', 'flying']
     },
 
     {
-    name: 'Ninetales',
-    height: 1.1,
-    type: ['fire']
+      name: 'Pikachu',
+      height: 1.04,
+      type: ['electric']
+    },
+
+    {
+      name: 'Ninetales',
+      height: 1.1,
+      type: ['fire']
     },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<br>' + pokemonList[i].name + (' , Height: ') + pokemonList[i].height);  
-    if (pokemonList[i].height >= 1.8) {
-      document.write(' - Wow, that\'s a big Pokemon!');
+function add (Pokemon) {
+  return pokemonList.push(pokemon)
+}
 
-      if (pokemonList[i].height <= 1.7 && pokemonList[i].height >= 1) {   
-        document.write(' - This is a medium size Pokemon!');
-      }
-      else if (pokemonList[i].height <= 1) {
-        document.write(' - This is a small Pokemon!');
-      }
+function getAll () {
+  return pokemonList
+}
 
-    }
+function showDetails (pokemon) {
+  console.log(pokemon.name)
+}
+
+Function addListItem(pokemon) {
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listPokemon = document.createElement("li");
+  let button = document.createElement("button");
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
+  listPokemon.appendChild(button);
+  pokemonList.appendChild(listPokemon);
+  addListener(button, pokemon);
+}
+
+return {
+  add: add,
+  getAll: getAll,
+  addListItem: addListitem,
+  showDetails: showDetails,
 }
