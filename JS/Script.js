4327c130903e9daa26bd8322bcd006d3988c46d2
Freecody15 <Dakota_pope1996@hotmail.com>
2022-04-06 21:45:23 -0400

    changed all code
    changed code to suit replit better and be cleaner looking

--- JS/Script.js
+++ JS/Script.js
@@ -1,73 +1,84 @@
-var pokemonRepository = (function () {
-  let repository = [
-    {
-      name: "Gyarados",
-      height: 6.5,
-      weight: 50,
-      type: ["Water", "flying"]
-    },
-    {
-      name: "Pikachu",
-      height: 1.04,
-      weight: 50,
-      type: ["electric"]
-    },
-    {
-      name: "Ninetales",
-      height: 1.1,
-      weight: 50,
-      type: ["fire"],
-    },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
 
-function add(pokemon) {
-  if (
-    typeof pokemon === "object" &&
-    "name" in pokemon &&
-    "height" in pokemon &&
-    "weight" in pokemon &&
-    "types" in pokemon
-  ) {
-    repository.push(pokemon);
-  } else {
-    console.log("pokemon is not correct");
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
-}
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
 
-function getAll () {
-  return repository;
-}
-function showDetails(pokemon) {
-  console.log(pokemon.name)
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
-function addListener(button, pokemon) {
-  button.addEventListener ("click", function () {
-    showDetails(pokemon)
-  })
-}
-function addListItem(pokemon) {
-  let pokemonList = document.querySelector(".pokemon-list");
-  let listpokemon = document.createElement("li");
-  let button = document.createElement("button");
-  button.innerText = pokemon.name;
-  button.classList.add("button-class");
-  listpokemon.appendChild(button);
-  pokemonList.appendChild(listpokemon);
-  addListener(button, pokemon);
-}
-return {
-  add: add,
-  getAll: getAll,
-  addListItem: addListItem,
-  showDetails: showDetails
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
   }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: "Eevee", height: 0.5, weight: 1.5, types: ["normal"] });
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
